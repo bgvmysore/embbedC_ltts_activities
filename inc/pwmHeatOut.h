58413d0087e7c7df383362be3944e1170932d053
bgvmysore <bgvmysore@gmail.com>
2021-04-29 15:29:04 +0530

    activity3 kind updates

--- inc/pwmHeatOut.h
+++ inc/pwmHeatOut.h
@@ -1,4 +1,14 @@
 #ifndef _PWMHEATOUT_H_
 #define _PWMHEATOUT_H_
 
+#ifndef __AVR_ATmega328__
+    #define __AVR_ATmega328__
+#endif
+
+#include <avr/io.h>
+
+void pwmInit_OC1A(void);
+
+void setCompare(uint16_t comparevalue);
+
 #endif//_PWMHEATOUT_H_
--- project_main.c
+++ project_main.c
@@ -11,15 +11,26 @@
 // AC0 used as analog input for temperature set control
 #include "tempSet.h"
 
+#include "pwmHeatOut.h"
+
 int main(void)
 {
 	uint16_t adcData = 0;
 	activity1_init();
 	initADC();
-	
+	pwmInit_OC1A();
+
 	while(1){
 		activity1_loop();
 		adcData = readADC0();
+		if(isHeaterActuated()){
+			setCompare(adcData);
+			_delay_ms(200);
+		}
+		else{
+			setCompare(0x0000);
+			_delay_ms(200);
+		}
 	}
 	
 	return 0;
